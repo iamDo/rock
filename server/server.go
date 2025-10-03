4eb21d055c599a3797e18c6551e66549c4e858f1
Pieter Polmans <pieter.polmans@protonmail.com>
2025-10-03 18:57:03 +0200

    add httpError struct and function to handle errors

--- server/server.go
+++ server/server.go
@@ -1,11 +1,21 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"rock/config"
 	"rock/tracker"
 )
+
+type httpError struct {
+	Msg  string
+	Code int
+}
+
+func (e *httpError) Error() string {
+	return fmt.Sprintf("Error [%d]: %s", e.Code, e.Msg)
+}
 
 func Serve() error {
 	http.HandleFunc("/start", handleStart)
@@ -47,4 +57,18 @@ func handleStop(w http.ResponseWriter, req *http.Request) {
 	comment := req.FormValue("comment")
 	fmt.Printf("STOP")
 	tracker.Stop(comment)
+}
+
+func handleHttpError(w http.ResponseWriter, err error) bool {
+	var httpError *httpError
+	if err != nil {
+		if errors.As(err, &httpError) {
+			http.Error(w, httpError.Msg, httpError.Code)
+			return true
+		} else {
+			http.Error(w, "Someting unexpected happened", http.StatusInternalServerError)
+			return true
+		}
+	}
+	return false
 }
